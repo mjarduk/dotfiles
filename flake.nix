f18010a9dcea1628acb94116ea381b9b55305597
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2026-06-18 19:54:19 +0300

    substituting flake.nix

--- flake.nix
+++ flake.nix
@@ -1,87 +1,29 @@
 {
-  description = "My dotfiles!! :D";
-
-  inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
-
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-  };
-
-  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, home-manager-unstable, agenix, ... }:
-  let
-    commonSettings = {
-      username = "mjarduk";
-      sshPubkeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP30FsTAhKNGSnDSXIK67xRmeVAzmzAoLzXa88r8hjEO mjarduk@marmar"
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBn0hysw8HdKRBT8500YrZwP0mqi1vrPztQxnbMkBuNEAAAABHNzaDo= mjarduk@gmail.com"
-      ];
-      password = "$6$FvuFvW5la529F.tQ$KG9jPlANaLVqw/L9MxsGii25oGQzY6LMoTTwmQeMlmV6vvT/0vGczIomIvPisC4tg9Z0tklrlkYqx6SKrMgkJ.";
-      bareMetal = false;
-    };
-  in{
-    homeConfigurations."mjarduk" = let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { homeDirectory = "/home/mjarduk"; inherit inputs; };
-      modules = [
-        agenix.homeManagerModules.default
-        ./home/mjarduk
-      ];
-    };
-
-    darwinConfigurations."marbook" = nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit self inputs; home-manager = home-manager-unstable; };
-      modules = [ ./hosts/marbook ];
-    };
-
-    nixosConfigurations = {
-      combine = let
-        settings = commonSettings // {
-          hostname = "combine";
-          vpnPort =   46780;
+        description = "NixOS from Hell >:)";
+        inputs = {
+                nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+                home-manager = {
+                        url = "github:nix-community/home-manager/release-26.05";
+                        inputs.nixpkgs.follows = "nixpkgs";
+                };
         };
-      in nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit settings; };
-        modules = [
-          { nixpkgs.hostPlatform = "x86_64-linux"; }
-          ./hosts/server/generic
-          ./hosts/server/generic/vmwguest.nix
-          ./hosts/server/combine
-          ./hardware/combine.nix
-          agenix.nixosModules.default
-        ];
-      };
-      minecraft = let
-        settings = commonSettings // {
-          hostname = "minecraft";
+
+        outputs = { self, nixpkgs, home-manager, ... }: {
+                nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+                        system = "x86_64-linux";
+                        modules = [
+                                ./hosts/nixos-btw/configuration.nix
+                                home-manager.nixosModules.home-manager
+                                {
+                                        home-manager = {
+                                                useGlobalPkgs = true;
+                                                useUserPackages = true;
+                                                users.kris = import ./home/default.nix;
+                                                backupFileExtension = "backup";
+                                        };
+                                }
+                        ];
+
+                };
         };
-      in nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit settings; };
-        modules = [
-          { nixpkgs.hostPlatform = "x86_64-linux"; }
-          ./hosts/server/generic
-          ./hosts/server/generic/vmwguest.nix
-          ./hosts/server/minecraft
-          ./hardware/minecraft.nix
-          agenix.nixosModules.default
-        ];
-      };
-    };
-  };
 }
