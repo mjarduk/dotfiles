1f180e5adfc9e7c3345f65d73e8ab0362d164cf0
mjarduk <mjarduk@gmail.com>
2026-06-23 12:15:40 +0300

    Add iGPU drivers for OpenCL to work

--- hosts/server/minecraft/default.nix
+++ hosts/server/minecraft/default.nix
@@ -1,6 +1,26 @@
-{ ... }: {
+{ settings, pkgs, ... }: {
   imports = [
       ./marcraft.nix
       ./servers.nix
+  ];
+
+  hardware.graphics = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+      vpl-gpu-rt
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    clinfo
+    pciutils
+  ];
+
+  users.users.${settings.username}.extraGroups = [
+    "render"
+    "video"
   ];
 }
