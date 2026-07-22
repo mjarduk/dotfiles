bfe506eda5597ecdac51e7c45f43fa31841d21e0
mjarduk <mjarduk@gmail.com>
2026-07-22 20:17:03 +0300

    Start zapusk server

--- hosts/server/minecraft/servers.nix
+++ hosts/server/minecraft/servers.nix
@@ -7,24 +7,25 @@
 
   systemd.tmpfiles.rules = [
     "d /srv/minecraft/zapusk 0744 minecraft minecraft -"
+    "d /srv/minecraft/zapusk-2026-08/ 0744 minecraft minecraft -"
     "d /srv/minecraft/gtnh 0744 minecraft minecraft -"
     "d /run/marcraft 0750 minecraft minecraft -"
   ];
 
-  # networking.firewall.allowedUDPPorts = [ 24454 ];
+  networking.firewall.allowedUDPPorts = [ 24454 ];
 
   services.marcraft.zapusk = {
-    enable = false;
+    enable = true;
 
-    dataDir = "/srv/minecraft/zapusk";
+    dataDir = "/srv/minecraft/zapusk-2026-08";
     javaPackage = pkgs.jdk25;
-    jvmFlags = ["-XX:+UseZGC" "-XX:+UseCompactObjectHeaders" "-XX:+UseStringDeduplication"];
-    forgeArgsPath = "libraries/net/minecraftforge/forge/1.20.1-47.4.20/unix_args.txt";
+    jvmFlags = ["-XX:+UseZGC" "-XX:+UseCompactObjectHeaders" "-XX:+UseStringDeduplication" "-Xmx4G"];
+    forgeArgsPath = "libraries/net/neoforged/neoforge/21.1.243/unix_args.txt";
     openFirewall = true;
   };
 
   services.marcraft.gtnh = {
-    enable = true;
+    enable = false;
 
     dataDir = "/srv/minecraft/gtnh";
     javaPackage = pkgs.jdk25;
