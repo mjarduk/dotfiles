5320132e74ccc0fd946916a81f32d05cc1d5786c
mjarduk <mjarduk@gmail.com>
2026-07-27 23:20:18 +0300

    Alter datadirv

--- hosts/server/minecraft/servers.nix
+++ hosts/server/minecraft/servers.nix
@@ -18,7 +18,7 @@
   services.marcraft.zapusk = {
     enable = true;
 
-    dataDir = "/srv/minecraft/zapusk-2026-08";
+    dataDir = "/mnt/universalis-data/zapusk-2026-08";
     javaPackage = pkgs.jdk25;
     jvmFlags = ["-XX:+UseZGC" "-XX:+UseCompactObjectHeaders" "-XX:+UseStringDeduplication" "-Xmx16G" "-Xms16G"];
     forgeArgsPath = "libraries/net/neoforged/neoforge/21.1.243/unix_args.txt";
